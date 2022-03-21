25c84b5d382dc22cee89fcd14c49c4bf87690b67
Voroz <jonny.fherm@gmail.com>
2022-03-21 14:36:11 +0100

    Centered button

--- feature_start/feature_start_presentation/src/main/java/ferm/jonny/feature_start_presentation/StartScreen.kt
+++ feature_start/feature_start_presentation/src/main/java/ferm/jonny/feature_start_presentation/StartScreen.kt
@@ -1,7 +1,14 @@
 package ferm.jonny.feature_start_presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination(start = true)
@@ -9,9 +16,16 @@ import com.ramcosta.composedestinations.annotation.Destination
 fun StartScreen(
     navigator: StartScreenNavigator
 ) {
-    Button(onClick = {
-        navigator.navigateToMovieOverviews()
-    }) {
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = {
+                navigator.navigateToMovieOverviews()
+            }) {
+            Text(text = "Go to movies")
+        }
     }
+
 }
